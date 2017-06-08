c55d88f3bea5d79f241a34e689c502e9f45a0ede
jedney0901 <josh_edney1@hotmail.com>
2017-06-08 13:51:05 +1000

    Create transaction working, editing and adding to come next

---
+++ src/data/BillsData.js
@@ -0,0 +1,32 @@
+const billsData = [
+  {
+    "transactionPurchaseDate": "29/05/2017",
+    "amount": -850,
+    "description": "ANZ INTERNET BANKING BPAY DEFT RENT                  ",
+    "frequencyOfTransaction": "Weekly",
+    "bill": true
+  },
+  {
+    "transactionPurchaseDate": "22/05/2017",
+    "amount": -39.9,
+    "description": "VISA DEBIT PURCHASE  AMAYSIM AUSTRALIA LIMI SYDNEY",
+    "frequencyOfTransaction": "Monthly",
+    "bill": true
+  },
+  {
+    "transactionPurchaseDate": "22/05/2017",
+    "amount": -39.9,
+    "description": "VISA DEBIT PURCHASE TPG AUSTRALIA LIMI SYDNEY",
+    "frequencyOfTransaction": "Fortnightly",
+    "bill": true
+  },
+  {
+    "transactionPurchaseDate": "22/05/2017",
+    "amount": -39.9,
+    "description": "VISA DEBIT PURCHASE INSURANCE",
+    "frequencyOfTransaction": "Yearly",
+    "bill": true
+  }
+]
+
+export default billsData;
